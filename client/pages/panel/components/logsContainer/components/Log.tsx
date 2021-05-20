332cbe3733c3b705ed9e957e6d6993222c75b773
Vitoudi <69259931+Vitoudi@users.noreply.github.com>
2021-05-20 10:40:30 -0300

    Update Log.tsx

--- client/pages/panel/components/logsContainer/components/Log.tsx
+++ client/pages/panel/components/logsContainer/components/Log.tsx
@@ -42,7 +42,7 @@ export default function Log({ log }: Props): ReactElement {
       >
         {getCorrespondingLogMsg(log?.eventName) || ''} -
       </span>
-      {log.channel}
+      {log?.channel}
       <span className={styles["date-place"]}>{getDateFromLog(log.date)}</span>
     </div>
   );
